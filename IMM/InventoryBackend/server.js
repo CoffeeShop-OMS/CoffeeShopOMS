c039d2a7b9fc700599ddb84c2781259f47050445
Mark Silla <marksilla12@gmail.com>
2026-03-04 01:02:02 +0800

    backend Setup

---
+++ IMM/InventoryBackend/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from 'cors'
+import 'dotenv/config'
+
+
+const app =express()
+const pot = process.env.PORT || 4000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Bukas sya ya!')
+})
+
+app.listen(pot, () => {
+    console.log(`Server is running on port ${pot}`)
+})
